443212402ab1fab6ebffaa10203c44adc6219667
Student <example@example.com>
2018-12-17 15:19:35 -0800

    begin to build out ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :places
+  # resources :places
 
   resources :users, only: [:index] do
     resources :messages, only: [:index, :create, :new, :show, :destroy]
