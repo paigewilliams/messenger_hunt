0db9c67e1c3ed0afba8476c7ae86dcbdc90b1527
Student <example@example.com>
2018-12-18 13:08:10 -0800

    new master pull request from here

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,22 +2,33 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :destroy]
 
   def index
+    @checkin = [session[:lat].to_f, session[:long].to_f]
     @inbox = []
     @outbox = []
+
+    @has_read_history = []
+
+    binding.pry
+
+    # @inbox = Message.where("to_user = #{current_user.id} AND read='true'")
+
     if current_user
       @inbox = Message.where("to_user = #{current_user.id}")
       @outbox = Message.where("from_user = #{current_user.id}")
     end
-    @checkin = [session[:lat].to_f, session[:long].to_f]
+
     @close_msg = []
+
     @inbox.each do |coor|
       distance = Haversine.distance(@checkin, [coor.msg_lat, coor.msg_long]).to_miles
       if distance < 0.1
-          @close_msg.push(coor)
+        coor.read = 'true'
+        coor.save
+        @close_msg.push(coor)
+        # binding.pry
         end
       end
-
-    end
+  end
 
 
 
